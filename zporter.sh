b9c67921430038b69e72ce63970c9e44b2499389
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2024-01-20 16:35:29 +0200

    كسم إسرائيل

--- zporter.sh
+++ zporter.sh
@@ -15,40 +15,40 @@ port_scan() {
 extract_domain() {
     local input=$1
 
-    local domain=$(echo "$input" | grep -oP '^(?:http:\/\/|https:\/\/)?\K[^\/\?\#]+')
-    
+    local domain=$(echo "$input" | grep -oP '^(?:http:\/\/|https:\/\/)?\K[^\/\?:#]+')
+
     echo "$domain"
 }
 
 while getopts "l:d:o:" opt; do
     case $opt in
-        l)
-            file="$OPTARG"
-            if [ ! -s "$file" ]; then
-                echo "File $file is missing or empty."
-                exit 1
-            fi
-            while IFS= read -r line; do
-                local domain=$(extract_domain "$line")
-                port_scan "$domain"
-            done < "$file"
-            ;;
-        d)
-            input="$OPTARG"
-            local domain=$(extract_domain "$input")
-            port_scan "$domain"
-            ;;
-        o)
-            output_file="$OPTARG"
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
+    l)
+        file="$OPTARG"
+        if [ ! -s "$file" ]; then
+            echo "File $file is missing or empty."
             exit 1
-            ;;
+        fi
+        while IFS= read -r line; do
+            local domain=$(extract_domain "$line")
+            port_scan "$domain"
+        done <"$file"
+        ;;
+    d)
+        input="$OPTARG"
+        local domain=$(extract_domain "$input")
+        port_scan "$domain"
+        ;;
+    o)
+        output_file="$OPTARG"
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
     esac
 done
 
-if [ $OPTIND -eq 1 ]; then 
+if [ $OPTIND -eq 1 ]; then
     echo "Usage: $0 -l <file> or $0 -d <input> [-o <output_file>]"
     exit 1
 fi
