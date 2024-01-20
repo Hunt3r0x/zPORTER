d505ed42b963752b731251fd129b2f287fc19267
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2024-01-20 21:45:10 +0200

    كسم إسرائيل

--- zporter.sh
+++ zporter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CYAN="\e[1;36m"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -22,6 +23,17 @@ prange="1 65535"
 #     eval "$command"
 # }
 
+displayusage() {
+    echo -e "${CYAN}OPTIONS:${NC}"
+    echo -e "  -d <domain or IP>            Specify a single domain or IP to scan"
+    echo -e "  -l <list of domains>         Specify a file containing a list of domains to scan"
+    echo -e "  -o <output file>             Specify the output file to save scan results"
+    echo -e "  -h                           Display this help message"
+    echo -e "${CYAN}EXAMPLE:"
+    echo -e "${YELLOW}      ./zporter.sh -d x.com -o out.txt${NC}"
+    echo -e "${YELLOW}      ./zporter.sh -l list.txt -o out.txt${NC}"
+}
+
 pscan() {
     local host=$1
 
@@ -39,29 +51,60 @@ filter() {
     echo "$input" | grep -oP '^(?:http:\/\/|https:\/\/)?\K[^\/\?:#]+' | sort -u # i realy hate this ):
 }
 
-while getopts "l:d:o:" opt; do
-    case $opt in
-    l)
-        file="$OPTARG"
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -d)
+        single_domain="$2"
+        shift
         ;;
-    d)
-        single_domain="$OPTARG"
+    -l)
+        file="$2"
+        if [ ! -f "$file" ]; then
+            echo -e "${RED}FILE NOT FOUND! --> $file${RESET}" >&2
+            exit 1
+        fi
+        shift
         ;;
-    o)
-        output="$OPTARG"
+    -o)
+        output="$2"
+        if [ -z "$output" ]; then
+            echo -e "${RED}NO OUTPUT FILE SPECIFIED!${RESET}" >&2
+            exit 1
+        fi
+        output_valid="true"
+        if [ ! -w "$(dirname "$output")" ]; then
+            echo -e "${RED}FILE PATH IS WRONG --> $output${RESET}" >&2
+            exit 1
+        fi
+        shift
+        ;;
+    -h)
+        displayusage
+        exit 0
         ;;
     \?)
-        echo -e "${RED}Invalid option:${NC} -$OPTARG" >&2
+        echo -e "${RED}INVALID OPTION -$OPTARG${RESET}" >&2
+        displayusage
+        exit 1
+        ;;
+    :)
+        echo -e "${RED}OPTION -$OPTARG REQUIRES AN ARGUMENT!${RESET}" >&2
+        displayusage
+        exit 1
+        ;;
+    *)
+        echo -e "${RED}FLAG PROVIDED BUT NOT DEFINED --> $1${RESET}" >&2
         exit 1
         ;;
     esac
+    shift
 done
 
 if [ -n "$file" ]; then
-    if [ ! -f "$file" ]; then
-        echo -e "${YELLOW}File $file does not exist.${NC}"
-        exit 1
-    fi
+    # if [ ! -f "$file" ]; then
+    #     echo -e "${YELLOW}File $file does not exist.${NC}"
+    #     exit 1
+    # fi
 
     while IFS= read -r line; do
         domain=$(filter "$line")
@@ -72,13 +115,5 @@ elif [ -n "$single_domain" ]; then
     domain=$(filter "$single_domain")
     pscan "$domain"
 else
-    echo -e "${GREEN}USAGE:${NC}"
-    echo -e "    ${YELLOW}./zporter.sh -l <file>${NC}"
-    echo -e "        domains from a file"
-    echo -e "    ${YELLOW}./zporter.sh -d <input> [-o <output>]${NC}"
-    echo -e "        single domain/IP"
-    echo -e "${GREEN}EXAMPLES:${NC}"
-    echo -e "    ${YELLOW}./zporter.sh -d x.com -o out.txt${NC}"
-    echo -e "    ${YELLOW}./zporter.sh -l list.txt -o out.txt${NC}"
-    exit 1
+    displayusage
 fi
