0a80f4bd86d50aaa6bfe7fe8e95c3270f6ad55bf
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2024-01-20 20:46:52 +0200

    كسم إسرائيل

--- zporter.sh
+++ zporter.sh
@@ -3,24 +3,35 @@
 output_file=""
 prange="1 65535"
 
+# pscan() {
+#     local host=$1
+#     local command
+
+#     command="seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u ${host}:{}"
+
+#     if [ -n "$output_file" ]; then
+#         command+=" | tee -a $output_file"
+#         # seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u "${host}:{}" | tee -a "$output_file"
+#     fi
+
+#     eval "$command"
+# }
+
 pscan() {
     local host=$1
-    local command
 
-    command="seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u ${host}:{}"
-
-    if [ -n "$output_file" ]; then
-        command+=" | tee -a $output_file"
-        # seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u "${host}:{}" | tee -a "$output_file"
+    if [ -n "$output" ]; then
+        echo -e "${GREEN} results will be saved --> ${YELLOW}${output}${NC}\n"
+        seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u ${host}:{} | tee -a "$output"
+    else
+        seq $prange | xargs -P 100 -I {} httpx -silent -sc -cl -title -u ${host}:{}
     fi
-
-    eval "$command"
 }
 
 extract_domain() {
     local input=$1
 
-    echo "$input" | grep -oP '^(?:http:\/\/|https:\/\/)?\K[^\/\?:#]+' | sort -u # i realy hate this ):
+    echo "$input" | grep -oP '^(?:http:\/\/|https:\/\/)?\K[^\/\?:#]+' | sort -u
 }
 
 while getopts "l:d:o:" opt; do
@@ -32,10 +43,10 @@ while getopts "l:d:o:" opt; do
         single_domain="$OPTARG"
         ;;
     o)
-        output_file="$OPTARG"
+        output="$OPTARG"
         ;;
     \?)
-        echo "Invalid option: -$OPTARG" >&2
+        echo -e "${RED}Invalid option:${NC} -$OPTARG" >&2
         exit 1
         ;;
     esac
@@ -43,7 +54,7 @@ done
 
 if [ -n "$file" ]; then
     if [ ! -f "$file" ]; then
-        echo "File $file does not exist."
+        echo -e "${YELLOW}File $file does not exist.${NC}"
         exit 1
     fi
 
@@ -56,7 +67,14 @@ elif [ -n "$single_domain" ]; then
     domain=$(extract_domain "$single_domain")
     pscan "$domain"
 else
-    echo "Usage: $0 -l <file> or $0 -d <input> [-o <output_file>]"
-    echo "Example: $0 -l list.txt or $0 -d x.com -o out.txt"
+    echo -e "${GREEN}USAGE:${NC}"
+    echo -e "    ${YELLOW}./zporter.sh -l <file>${NC}"
+    echo -e "        domains from a file"
+    echo -e "    ${YELLOW}./zporter.sh -d <input> [-o <output>]${NC}"
+    echo -e "        single domain/IP"
+    echo -e ""
+    echo -e "${GREEN}EXAMPLES:${NC}"
+    echo -e "    ${YELLOW}./zporter.sh -d x.com -o out.txt${NC}"
+    echo -e "    ${YELLOW}./zporter.sh -l list.txt -o out.txt${NC}"
     exit 1
 fi
